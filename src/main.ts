1d1649b3415f56aa37b473fc54981a98f5d5298a
hanzalaredtilt <ahmed.hanzala@redtilt.team>
2024-10-13 18:30:47 +0500

    new

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,4 @@
 // const server = app.listen(port, () => {
 //   console.log(`Listening at http://localhost:${port}/api`);
 // });
-// server.on('error', console.error);
+// server.on('error', console.e
